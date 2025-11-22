89af8a2f809bc4852fc7093488f0150cdff4314b
AhmedDhibi1 <ahmed.dhibi@etudiant-enit.utm.tn>
2025-11-22 13:14:00 +0100

    feat(webui): scaffold Spring Boot web UI adapter module

---
+++ src/bluezone-adapter-forparkingcars-webui/src/main/bluezone.adapter.forparkingcars.webui/io/github/proj/bluezone/adapter/forparkingcars/webui/WebUISpringBootApp.java
@@ -0,0 +1,22 @@
+package io.github.proj.bluezone.adapter.forparkingcars.webui;
+
+import io.github.proj.bluezone.hexagon.ports.driving.forparkingcars.ForParkingCars;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
+
+
+@SpringBootApplication
+public class WebUISpringBootApp {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(WebUISpringBootApp.class).web(WebApplicationType.SERVLET).run(args);
+    }
+
+    @Bean
+    public ForParkingCars carParker() {
+        return ForParkingCarsWebUIDriver.carParker();
+    }
+
+}
---
+++ src/bluezone-adapter-forparkingcars-webui/src/main/bluezone.adapter.forparkingcars.webui/module-info.java
@@ -0,0 +1,25 @@
+open module bluezone.adapter.forparkingcars.webui {
+// Opened because Thymeleaf needs access to templates resources at runtime
+
+	// DEPENDS ON
+	requires bluezone.hexagon;
+	requires io.github.proj.lib.portsadapters;
+	requires spring.boot.starter.web;
+	requires spring.boot.starter.thymeleaf;
+	requires bootstrap;
+	requires spring.boot;
+	requires spring.boot.autoconfigure;
+	requires spring.context;
+	requires spring.beans;
+	requires spring.web;
+	requires org.apache.tomcat.embed.core;
+	requires thymeleaf;
+	requires thymeleaf.spring5;
+	requires thymeleaf.extras.java8time;
+    requires spring.webmvc;
+	requires spring.core;
+
+    // PUBLISHES
+	exports io.github.proj.bluezone.adapter.forparkingcars.webui;
+
+}
